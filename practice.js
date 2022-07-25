ac0ff73d0850965e955e3b3890fea28d4e5bbd72
Divyansh-jft <108716321+Divyansh-jft@users.noreply.github.com>
2022-07-25 22:43:59 +0530

    Delete practice.js

--- practice.js
+++
@@ -1,9 +0,0 @@
-for (let i = 100; i <= 999; i++) {
-    let a= String();
-    let arrayN= a.split('')
-    if ((Number(arrayN[0] )+ Number(arrayN[1 ])) === Number
-    (arrayN[2])){
-        console.log(arrayN);
-    }
-    }
-
